e754e18408807be36491e540f44f6d05d1c1f579
DevVm\jeremy <jeremydaniel.bouvier@gmail.com>
2017-05-07 16:48:39 -0400

    save

---
+++ FichesMaths/src/FichesMaths/Controllers/FichesController.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
+
+namespace FichesMaths.Controllers
+{
+    [Route("api")]
+    public class FichesController : Controller
+    {
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+        public FichesController(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        [HttpGet("demonstration-second-degre")]
+        public virtual FileResult FicheSecondDegre()
+        {
+
+            var bytes = System.IO.File.ReadAllBytes($"{ _hostingEnvironment.WebRootPath}\\Fiche-Démonstration-Second-degré.pdf");
+            return File(bytes,"application/pdf");
+
+        }
+    }
+}
---
+++ FichesMaths/src/FichesMaths/Middlewares/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Diagnostics;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace FichesMaths.Middlewares
+{
+    public class ExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            try
+            {
+                await _next.Invoke(httpContext).ConfigureAwait(false);
+            }
+            catch (Exception ex)
+            {
+
+                try
+                {
+                    Trace.WriteLine($"Exception message: {ex.Message}");
+                    Trace.WriteLine($"Exception stack Trace: {ex.StackTrace}");
+                }
+                catch (Exception)
+                {
+                    // Missing Logger
+                }
+                httpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                await httpContext.Response.WriteAsync(HttpStatusCode.InternalServerError.ToString()).ConfigureAwait(false);
+            }
+
+        }
+
+    }
+    public static class ExceptionHandlerMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseExceptionHandlerMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ExceptionHandlerMiddleware>();
+        }
+    }
+}
